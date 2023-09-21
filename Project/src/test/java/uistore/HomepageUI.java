64aa402b69144f8f76de5c1114e5c837e55ea596
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:16:38 +0000

    Backup Commit

--- Project/src/test/java/uistore/HomepageUI.java
+++ Project/src/test/java/uistore/HomepageUI.java
@@ -6,7 +6,7 @@ public class HomepageUI {
     // public static By username = By.name("username");
     public static By username = By.xpath("//*[@id=\"username\"]");
     public static By password = By.id("password");
-    public static By submit = By.id("submit");
-    public static By submit = By.id("submit");
+    // public static By submit = By.id("submit");
+    public static By submit = By.xpath("//*[@id=\"submit\"]");
    
 }
